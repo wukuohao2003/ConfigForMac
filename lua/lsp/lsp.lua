44b02f4891151cf72472de5703c2e8615cdaa357
wukuohao <wukuohao2022@icloud.com>
2024-08-05 19:35:37 +0800

    change lsp hl

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -7,16 +7,18 @@ if not status then
 	return false
 end
 
+-- 判断光标前是否有字符
 local has_words_before = function()
 	unpack = unpack or table.unpack
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
 	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
+-- 报错图标设置
 local signs = { Error = " 󰬌", Warn = " 󰬞", Info = " 󰬐", Hint = " 󰬔" }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 function M.Config()
